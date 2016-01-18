a3a20ca93a48487e34692ec5222d5283f4f8ec10
yanyiwu <i@yanyiwu.com>
2016-01-18 23:30:57 +0800

    update demo

--- example/demo.go
+++ example/demo.go
@@ -7,10 +7,19 @@ import (
 	"github.com/yanyiwu/gojieba"
 )
 
-func main() {
+const (
+	DICT_PATH       = "../dict/jieba.dict.utf8"
+	HMM_PATH        = "../dict/hmm_model.utf8"
+	USER_DICT_PATH  = "../dict/user.dict.utf8"
+	IDF_PATH        = "../dict/idf.utf8"
+	STOP_WORDS_PATH = "../dict/stop_words.utf8"
+)
+
+func DemoJieba() {
+	fmt.Println("DemoJieba")
 	var words []string
 	use_hmm := true
-	x := gojieba.New("../dict/jieba.dict.utf8", "../dict/hmm_model.utf8", "../dict/user.dict.utf8")
+	x := gojieba.NewJieba(DICT_PATH, HMM_PATH, USER_DICT_PATH)
 	defer x.Free()
 
 	words = x.CutAll("我来到北京清华大学")
@@ -24,4 +33,20 @@ func main() {
 
 	words = x.CutForSearch("小明硕士毕业于中国科学院计算所，后在日本京都大学深造", use_hmm)
 	fmt.Println("搜索引擎模式:", strings.Join(words, "/"))
+}
+
+func DemoExtract() {
+	fmt.Println("DemoExtract")
+	x := gojieba.NewExtractor(DICT_PATH, HMM_PATH, USER_DICT_PATH, IDF_PATH, STOP_WORDS_PATH)
+	defer x.Free()
+
+	s := "我是拖拉机学院手扶拖拉机专业的。不用多久，我就会升职加薪，当上CEO，走上人生巅峰。"
+	words := x.Extract(s, 5)
+	fmt.Println(s)
+	fmt.Println("关键词抽取:", strings.Join(words, "/"))
+}
+
+func main() {
+	DemoJieba()
+	DemoExtract()
 }
