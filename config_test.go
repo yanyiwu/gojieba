9979422c3c2ac8899a0f3af13fc715433add5a10
萧江南 <xiaojiangnan@xiaojiangnandeMacBook-Pro.local>
2020-12-28 10:39:13 +0800

    修改config_test

--- config_test.go
+++ config_test.go
@@ -8,7 +8,3 @@ func TestConfig(t *testing.T) {
 	file := getCurrentFilePath()
 	println(file)
 }
-
-func TestGetDictPaths(t *testing.T) {
-	println(getDictPaths(DictPath("dictpath"), IdfPath("idfPath"), "user_dict_path"))
-}
