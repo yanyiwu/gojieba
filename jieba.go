d75c19edaba13d66522b27447502e73867bae9ce
yanyiwu <wuyanyi09@gmail.com>
2024-07-19 10:26:41 +0800

    [CXXFLAGS] add -Wno-unused-variable to reduce WARNING LOGS

--- jieba.go
+++ jieba.go
@@ -1,7 +1,7 @@
 package gojieba
 
 /*
-#cgo CXXFLAGS: -I./deps -DLOGGING_LEVEL=LL_WARNING -O3 -Wall  -Wno-deprecated
+#cgo CXXFLAGS: -I./deps -DLOGGING_LEVEL=LL_WARNING -O3 -Wno-deprecated -Wno-unused-variable
 #include <stdlib.h>
 #include "jieba.h"
 */
